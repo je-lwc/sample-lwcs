ccf930e92909a16d6a7203566990c8d5813ba6d0
je-lwc <79720377+je-lwc@users.noreply.github.com>
2022-01-07 14:15:04 -0500

    'save'

--- src/lwc/pizzaBuilder/pizzaBuilder.js
+++ src/lwc/pizzaBuilder/pizzaBuilder.js
@@ -1,7 +1,20 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {
-  sizeOptions = [{ label: 'Small', value: "10''" }];
+  sizeOptions = [
+    { label: 'Small', value: '10"' },
+    { label: 'Medium', value: '12"' },
+    { label: 'Large', value: '14"' },
+    { label: 'Extra Large', value: '16"' }
+  ];
+  crustOptions = [
+    { label: 'Thin Crust', value: 'Thin' },
+    { label: 'Original Crust', value: 'Original' },
+    { label: 'Crunchy Crispy', value: 'Crunchy' }
+  ];
+
+  size = '10"';
+  crust = 'Original';
 
   handleSizeSelection() {}
 }
