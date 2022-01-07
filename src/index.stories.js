8e6303ec17f81bef71566c7dba3f3cd1d9dd02ac
je-lwc <79720377+je-lwc@users.noreply.github.com>
2022-01-07 14:07:21 -0500

    'save'

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,6 +1,6 @@
-import "@lwc/synthetic-shadow";
-import "https://unpkg.com/@salesforce-ux/design-system@2.13.1/assets/styles/salesforce-lightning-design-system.min.css";
-import { createElement } from "lwc";
-import App from "./app";
+import '@lwc/synthetic-shadow';
+import 'https://unpkg.com/@salesforce-ux/design-system@2.13.1/assets/styles/salesforce-lightning-design-system.min.css';
+import { createElement } from 'lwc';
+import App from './lwc/app';
 
-export const story = () => createElement("c-app", { is: App });
+export const story = () => createElement('c-app', { is: App });
